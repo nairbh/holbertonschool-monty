e72081827bccde0ef646526f7af8d3d1b9af69ed
nairbh <houcinenair@proton.me>
2023-05-12 13:42:52 +0200

    unknown command

--- functions.c
+++ functions.c
@@ -36,6 +36,12 @@ void execute_instructions(FILE *file, stack_t **stack)
 				instructions[i].f(stack, line_number);
 				break;
 			}
+
+			if (instructions[i].opcode == NULL)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			exit(EXIT_FAILURE);
+		}
 		}
 	}
 
